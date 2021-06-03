56ede47741a2bf7e40ee4695a6d5a8f719f0443d
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-06-03 11:07:40 -0400

    Added basic CRUD methods and index view file for the Aircraft, Airport, and Flight model classes

--- app/controllers/aircraft_controller.rb
+++ app/controllers/aircraft_controller.rb
@@ -1,2 +1,24 @@
 class AircraftController < ApplicationController
+
+    def index
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    def show
+    end
+
 end
--- app/controllers/airport_controller.rb
+++ app/controllers/airport_controller.rb
@@ -1,2 +1,24 @@
 class AirportController < ApplicationController
+
+    def index
+    end
+
+    def new
+    end
+
+    def edit
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    def show
+    end
+
 end
