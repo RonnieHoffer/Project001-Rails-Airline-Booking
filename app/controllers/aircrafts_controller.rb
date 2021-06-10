8f2f4f855237c41d4a50b8a4ac8d4df4f9fee1ed
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-06-09 21:01:04 -0400

    First commit of June 9, 2021

--- app/controllers/aircrafts_controller.rb
+++ app/controllers/aircrafts_controller.rb
@@ -14,14 +14,11 @@ class AircraftsController < ApplicationController
 
     def create
         @aircraft = Aircraft.new(aircraft_params)
-        # @aircraft.model = params[:model]
-        # @aircraft.economy_seats = params[:economy_seats]
-        # @aircraft.business_seats = params[:business_seats]
-        # @aircraft.first_class_seats = params[:first_class_seats]
         if @aircraft.save
             redirect_to '/aircrafts'
         else
             render plain: "Sorry.  Aircraft was not created."
+            # render :new
         end
     end
 
