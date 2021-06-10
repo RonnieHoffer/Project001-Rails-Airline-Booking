439262bcfff40d9bd09016777ec43e720d4c9a9f
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-06-09 21:23:03 -0400

    Switched routes to RESOURCES format

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get 'aircrafts' => 'aircrafts#index'
-  get 'aircrafts/new' => 'aircrafts#new'
-  post 'aircrafts' => 'aircrafts#create'
-  get 'aircrafts/:id' => 'aircrafts#show'
-  get 'aircrafts/:id/edit/' => 'aircrafts#edit'
-  patch 'aircrafts/:id' => 'aircrafts#update'
-
-
-
-  get 'airports' => 'airports#index'
-
-
-
-  get 'flights' => 'flights#index'
+  resources  :aircrafts
+  resources  :airports
+  resources  :flights
+  resource   :user
+  resources  :bookings
 
 end
