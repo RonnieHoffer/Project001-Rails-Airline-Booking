0f9fc76153dc5fda5ce34478cccbe58adb63e081
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-06-13 18:09:21 -0400

    Created index and show methods and views.  Started creating new/create methods and views, but they are not completed.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,15 +1,24 @@
 class FlightsController < ApplicationController
 
     def index
+        @flights = Flight.all
     end
 
     def new
+        @flight = Flight.new
     end
 
     def edit
     end
 
     def create
+        @flight = Flight.new(flight_params)
+        if @flight.save
+            redirect_to flights_url
+        else
+            render plain: "Sorry.  Flight was not created."
+            #render :new
+        end
     end
 
     def update
@@ -19,6 +28,27 @@ class FlightsController < ApplicationController
     end
 
     def show
+        @flight = Flight.find(params[:id])
+        @aircraft = Flight.find(params[:id]).aircraft
+        a = Flight.find(params[:id]).depart_airport_id
+        @departure_airport = Airport.find(a)
+        b = Flight.find(params[:id]).arriv_airport_id
+        @arrival_airport = Airport.find(b)
+
+    end
+
+
+    private
+    def flight_params
+        params.require(:flight).permit(
+            :flight_number, 
+            :aircraft_id, 
+            :depart_airport_id, 
+            :arriv_airport_id, 
+            :date, 
+            :depart_time, 
+            :arrive_time
+        )
     end
 
 end
